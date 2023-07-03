3df723e4ab359ea1601d5374e6983e0660861f28
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2023-07-03 14:40:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 from typing import Optional
 
+import requests
 from fastapi import FastAPI
+from fastapi import HTTPException
+from PyPDF2 import PdfReader
 
 app = FastAPI()
 
+@app.post('/extract_text')
+async def extract_text(url: str):
+    # Download the PDF file
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Failed to download PDF")
 
+    with open('temp.pdf', 'wb') as f:
+        f.write(response.content)
+
+    # Open the PDF file and extract text
+    with open('temp.pdf', 'rb') as f:
+        reader = PdfReader(f)
+        num_pages = len(reader.pages)
+        text = ''
+        for page in range(num_pages):
+            page_obj = reader.pages[page]
+            text += page_obj.extract_text()
+
+    # Delete the temporary PDF file
+    # Comment out the following line if you want to keep the downloaded file
+    import os
+    os.remove('temp.pdf')
+
+    return {'text': text}
+    
 @app.get("/")
 async def root():
     return {"message": "Hello Vasya"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
